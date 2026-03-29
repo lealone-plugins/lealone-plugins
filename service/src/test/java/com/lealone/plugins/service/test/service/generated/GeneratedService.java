cec0fb931e98768ede8d35ecca8fa045685f4e84
Honghua Zhu <zhh200910@gmail.com>
2026-03-29 16:29:05 +0800

    删除service模块

--- service/src/test/java/com/lealone/plugins/service/test/service/generated/GeneratedService.java
+++
@@ -1,8 +0,0 @@
-package com.lealone.plugins.service.test.service.generated;
-
-public class GeneratedService {
-
-    public Integer test(String name) {
-        return null;
-    }
-}
--- service/src/test/resources/lealone-test.sql
+++
@@ -1,7 +0,0 @@
--- Copyright Lealone Database Group.
--- Licensed under the Server Side Public License, v 1.
--- Initial Developer: zhh
-
-create config lealone (
-    base_dir: 'target/test-data'
-)
